f019c8ee2a7369ee9b39e8a52428797ae5d2cd17
Greg Munro <gregmunro90@gmail.com>
2022-08-24 10:03:04 +0200

    bookmark branch and component init

---
+++ src/components/bookmark/index.js
@@ -0,0 +1,10 @@
+/* global WebsyDesigns */ 
+class Bookmark {
+  constructor (elementId, options) {
+    this.elementId = elementId
+    this.options = Object.assign({}, options)
+    this.optionDefaults = {
+      
+      }      
+    }
+  }
